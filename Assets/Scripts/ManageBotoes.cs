a09a644165677472249acc1ea41b057f8d422cad
Leonardo Fernandes <73710738+Leotreew@users.noreply.github.com>
2022-03-03 17:52:42 -0300

    Modificação Leo

--- Assets/Scripts/ManageBotoes.cs
+++ Assets/Scripts/ManageBotoes.cs
@@ -5,20 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class ManageBotoes : MonoBehaviour
 {
+    public string nomeDaCena;
+
     // Start is called before the first frame update
     void Start()
     {
         PlayerPrefs.SetInt("score", 0);
     }
-
-    // Update is called once per frame
-    void Update()
+    // Chama o ChangeScene pra conseguir ter um delay  com o som do botao
+    public void ChangeS()
     {
-        
+        Invoke("ChangeScene", 0.2f);
     }
-
-    public void StartMundoGame()
+    // Troca basica de cena podendo ser usada em outros botoes com outras cenas
+    public void ChangeScene()
     {
-        SceneManager.LoadScene("Lab1");
+        SceneManager.LoadScene(nomeDaCena);
     }
 }
